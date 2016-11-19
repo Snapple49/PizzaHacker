28a719badd584ef96c0753aa10a5e777d8938c84
Michael <m.dekaste@student.ru.nl>
2016-11-19 07:58:03 +0100

    changes

--- wear/src/main/java/com/hackergames/hackathon/pizzahacker/MainActivity.java
+++ wear/src/main/java/com/hackergames/hackathon/pizzahacker/MainActivity.java
@@ -1,8 +1,14 @@
 package com.hackergames.hackathon.pizzahacker;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -18,7 +24,17 @@ public class MainActivity extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+
+                Button button = (Button)findViewById(R.id.codeAcceptButton);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EditText t = (EditText)findViewById(R.id.inputCode);
+                        System.out.println(t.getText());
+                    }
+                });
             }
         });
+
     }
 }
