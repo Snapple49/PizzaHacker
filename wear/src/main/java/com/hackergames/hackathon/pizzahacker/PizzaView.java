f9218f859dad5478b33c6963fb768fdc2b8f7248
Snapple49 <oliver-dot-stein@hotmail.com>
2016-11-19 06:57:10 +0100

    Created PizzaView class

---
+++ wear/src/main/java/com/hackergames/hackathon/pizzahacker/PizzaView.java
@@ -0,0 +1,21 @@
+package com.hackergames.hackathon.pizzahacker;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * Created by olive on 2016-11-19.
+ */
+
+class PizzaView extends View {
+
+    public PizzaView(Context context) {
+        super(context);
+    }
+
+    public void onDraw(Canvas canvas){
+
+    }
+
+}
